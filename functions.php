b7d0691d94d9c475b6e012df3ef83e66f4223ac4
xmartina <martinsprince126@gmail.com>
2025-04-02 15:29:34 +0100

    d

--- functions.php
+++ functions.php
@@ -1,17 +1,12 @@
 <?php
 function wallet_name_1($result_wallet){
-    while ($row = $result_wallet->fetch_assoc()){  // Use fetch_assoc() for MySQLi
+    while ($row = $result_wallet->fetch(PDO::FETCH_ASSOC)){
         $wallet_id = $row['id'];
         $wallet_name = $row['name'];
         $wallet_page_id = $row['page_id'];
-
-        if ($wallet_page_id == 1){  // Fixed condition (use == instead of =)
-            ?>
-            <div class="wallet-item" data-wallet-id="<?=$wallet_id?>">
-                <img src="./img/wallet_img/<?=$wallet_id?>.png" alt="Binance">
-                <span><?=$wallet_name?></span>
-            </div>
-            <?php
-        }
+        if ($wallet_page_id == 1){
+        ?>
+        <div class="wallet-item" data-wallet-id="<?=$wallet_id?>"><img src="./img/wallet_img/<?=$wallet_id?>.png" alt="Binance"><span><?=$wallet_name?></span></div>
+<?php }
     }
 }
